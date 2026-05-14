a883dffcc505d673e6d6f89d84a28ac37904f8b7
kanan27-crypto <kanan27kanu@gmail.com>
2026-05-14 09:30:51 +0530

    PrintableDocument

---
+++ Main91.java
@@ -0,0 +1,19 @@
+interface Printable {
+
+    void print();
+}
+
+class Document implements Printable {
+
+    public void print() {
+        System.out.println("Printing document");
+    }
+}
+
+public class Main91 {
+    public static void main(String[] args) {
+
+        Document d = new Document();
+        d.print();
+    }
+}
