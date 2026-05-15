6c41de522ad1d3e9d7eec9143b6991b66b13ac08
kanan27-crypto <kanan27kanu@gmail.com>
2026-05-15 22:08:15 +0530

    ExceptionalHandling

---
+++ Main66.java
@@ -0,0 +1,17 @@
+public class Main66 {
+    public static void main(String[] args) {
+
+        try {
+            int a = 10 / 0;
+            System.out.println(a);
+        }
+
+        catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+
+        finally {
+            System.out.println("Program ended");
+        }
+    }
+}
